f0c8771078b7584a8edd93a58f34eec97658d3b3
khusniridh0 <rmoh8394@gmail.com>
2026-02-26 11:37:17 +0700

    include SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-// import { Analytics } from '@vercel/analytics/next';
-// import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "@/styles/main.css";
@@ -56,10 +56,10 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
 		<html lang="id">
 			<body className={`${montserrat.variable} antialiased`}>
 				{children}
-				{/* {process.env.NODE_ENV === 'production' && (<>
+				{process.env.NODE_ENV === 'production' && (<>
 					<SpeedInsights />
 					<Analytics />
-				</>)} */}
+				</>)}
 			</body>
 		</html>
 	);
